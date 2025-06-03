fa32d74eaf823d72b284dc20ec8327ee76ed1109
karen-valdes-tuliz <karen.ariadna1@gmail.com>
2025-06-02 23:04:26 -0300

    Elimine unos archivos que estaban de mas

--- clase8/ejemploclase/operadores.js
+++
@@ -1,9 +0,0 @@
-//operadores aritmeticos
-let a = 10;
-let b = 3;
-let suma = a +b;
-let resta a - b;
-let multiplicacion = a * b;
-let division = a / b;
-let modulo = a % b;
-
--- clase8/ejemploclase/variable_constante.js
+++
@@ -1,6 +0,0 @@
-// Declarar variables de tipo let y const
-let nombre = "Karen";
-conts edad = 25;
-
-console.log ("Hola, mi nombre es:"+ nombre + "y tengo" + edad + "años.")
-console.log (´hola, mi nombre es ${nombre} y tengo ${edad} años.´)
